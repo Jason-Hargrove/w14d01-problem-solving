3476b3219df9c64fa9a71d03213d1cd1048f928a
Jason Hargrove <rijaha@gmail.com>
2021-07-20 18:44:28 -0500

    Palindrome completed

--- solutions.js
+++ solutions.js
@@ -1,3 +1,4 @@
+// ===== Verbal Questions =====
 // What is the difference between a parameter and an argument?
 
 // Answer: A parameter is a variable in the function definition. An argument is a real value supplied to (passed to) the function. The paramieter are initialized to the value of the arguments it takes in.
@@ -6,3 +7,17 @@
 // Within a function, what is the difference between return and console.log?
 
 // Answer: return returns some value. console.log prints information to the console and is used for debugging.
+
+
+// ===== Sum Array =====
+const checkPalindrome = (str) => {
+  let lowerCaseStr = str.toLowerCase();
+  let reverseStr = lowerCaseStr.split('').reverse().join('');
+  if (lowerCaseStr === reverseStr) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkPalindrome('Radar'));
+console.log(checkPalindrome("Borscht"));
